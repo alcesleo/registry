a79f3b725eaa8eb0e3281db5650344f5beee9833
alcesleo <lagginglion@gmail.com>
2013-10-03 18:17:24 +0200

    Fix standards

--- src/Registry/Models/BoatModel.php
+++ src/Registry/Models/BoatModel.php
@@ -4,63 +4,63 @@ namespace Registry\Models;
 
 class BoatModel
 {
-	private $boatID;
-	private $ownerID;
-	private $boatType;
-	private $length;
+    private $boatID;
+    private $ownerID;
+    private $boatType;
+    private $length;
 
     public function __construct($boatID, $ownerID, $boatType, $length)
     {
-    	$this->setBoatID($boatID);
-    	$this->setOwnerID($ownerID);
-    	$this->setBoatType($boatType);
-    	$this->setLength($length);
+        $this->setBoatID($boatID);
+        $this->setOwnerID($ownerID);
+        $this->setBoatType($boatType);
+        $this->setLength($length);
     }
 
-    private function setBoatID($boatID) 
+    private function setBoatID($boatID)
     {
-    	if (!is_numeric($boatID) {
+        if (! is_numeric($boatID)) {
             throw new \Exception("BoatID must be numeric");
         }
-    	$this->boatID = $boatID;
+        $this->boatID = $boatID;
     }
 
-    private function setOwnerID($ownerID) 
+    private function setOwnerID($ownerID)
     {
-    	if (!is_numeric($ownerID) {
+        if (! is_numeric($ownerID)) {
             throw new \Exception("OwnerID must be numeric");
         }
-    	$this->ownerID = $ownerID;
+        $this->ownerID = $ownerID;
     }
 
-    private function setBoatType($boatType) 
+    private function setBoatType($boatType)
     {
-    	//Validation here
-    	$this->boatType = $boatType;
+        //Validation here
+        $this->boatType = $boatType;
     }
 
-    private function setLength($length) 
+    private function setLength($length)
     {
-    	//Validation here
-    	$this->length = $length;
+        //Validation here
+        $this->length = $length;
     }
 
-    public function getBoatID() 
+    public function getBoatID()
     {
         return $this->boatID;
     }
 
-    public function getOwnerID() 
+    public function getOwnerID()
     {
         return $this->ownerID;
     }
 
-    public function getBoatType() 
+    public function getBoatType()
     {
         return $this->boatType;
     }
 
-    public function getLength() 
+    public function getLength()
     {
         return $this->length;
     }
