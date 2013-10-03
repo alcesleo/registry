1d5f7563f592a332655651f1b31c7e4557412e35
alcesleo <lagginglion@gmail.com>
2013-10-03 17:47:42 +0200

    Fix unnecessary comments, fix indentation by tabs

--- src/Registry/Views/SingleMemberView.php
+++ src/Registry/Views/SingleMemberView.php
@@ -6,43 +6,40 @@ use Registry\Models\MemberModel;
 
 class SingleMemberView
 {
-	/**
-	 * @param MemberModel $member
-	 * @var string $memberID taken from MemberModel $member
-	 * @var string $name taken from MemberModel $member
-	 * @var string $ssn taken from MemberModel $member
-	 */
-	public function printMemberData(MemberModel $member) 
-	{
-		$memberID = $member->getMemberID();
-		$name = $member->getName();
-		$ssn = $member->getSocialSecurityNumber();
-		
-		print "
-					\n ----- Member Information -----
-					\n MemberID : $memberID 
-					\n Name : $name 
-					\n SSN : $ssn
-					\n";
-		
-		$this->showMenuOptions();
-	}
-	
-	private function showMenuOptions() 
-	{
-		$options = array(
+    /**
+     * @param MemberModel $member
+     */
+    public function printMemberData(MemberModel $member)
+    {
+        $memberID = $member->getMemberID();
+        $name = $member->getName();
+        $ssn = $member->getSocialSecurityNumber();
+
+        print "
+        			\n ----- Member Information -----
+        			\n MemberID : $memberID
+        			\n Name : $name
+        			\n SSN : $ssn
+        			\n";
+
+        $this->showMenuOptions();
+    }
+
+    private function showMenuOptions()
+    {
+        $options = array(
             'e' => 'Edit this member',
             'd' => 'Delete this memeber',
             'r' => 'Return to main menu'
         );
         $view = new MenuView($options);
         $this->doAction($view->readMenuOption());
-	}
-	
-	/**
-	 * @param string $option [vald key]
-	 */
-	private function doAction($option)
+    }
+
+    /**
+     * @param string $option [vald key]
+     */
+    private function doAction($option)
     {
         switch ($option) {
             case 'e':
@@ -57,4 +54,3 @@ class SingleMemberView
         }
     }
 }
-	
