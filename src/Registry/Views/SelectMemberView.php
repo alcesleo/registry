0069f98e8cae2ce6c0772cd63c00a602b225e5e9
alcesleo <lagginglion@gmail.com>
2013-10-03 20:03:50 +0200

    Fix conversion to spaces

--- src/Registry/Views/SelectMemberView.php
+++ src/Registry/Views/SelectMemberView.php
@@ -7,10 +7,10 @@ use Registry\Models\MemberModel;
 
 class SelectMemberView
 {
-	public function getSelectedMember() 
-	{
-		
-		$memberID = array(
+    public function getSelectedMember()
+    {
+
+        $memberID = array(
             '1' => 'Johan Andersson',
             '2' => 'Stefan Karlsson',
             '3' => 'Reggie Jacksson',
@@ -19,20 +19,19 @@ class SelectMemberView
         );
         $menu = new MenuView($memberID);
         $member = $this->getMemberObject($menu->readMenuOption("Please select user: "));
-		
-		return $member;
-	}
-	
-	/**
-	 * @param integer $memberID [contains memberID from selected member]
-	 * @var MemberModel $member
-	 */
-	private function getMemberObject($memberID) 
-	{
-		//TODO: Fix database-conn. Right now all return fake object
-		$member = new MemberModel(1, "Johan", 1234567890);
-		
-		return $member;
-	}
+
+        return $member;
+    }
+
+    /**
+     * @param integer $memberID [contains memberID from selected member]
+     * @var MemberModel $member
+     */
+    private function getMemberObject($memberID)
+    {
+        //TODO: Fix database-conn. Right now all return fake object
+        $member = new MemberModel(1, "Johan", 1234567890);
+
+        return $member;
+    }
 }
-	
