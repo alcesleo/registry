917d8a7ddc2783016f74544e9d08ee81a7185307
elinlix <el222fr@student.lnu.se>
2013-10-03 10:13:10 +0200

    Model bases

--- src/Registry/Models/MemberModel.php
+++ src/Registry/Models/MemberModel.php
@@ -4,8 +4,36 @@ namespace Registry\Models;
 
 class MemberModel
 {
-    public function __construct()
-    {
+	private $memberID;
+	private $name;
+	private $socialSecurityNumber;
 
+    public function __construct($memberID, $name, $socialSecurityNumber)
+    {
+        $this->setMemberID($memberID);
+    	$this->setName($name);
+        $this->setSocialSecurityNumber($socialSecurityNumber);
+    }
+
+    private function setMemberID($memberID) 
+    {
+        if (!is_numeric($memberID)) {
+            throw new \Exception("MemberID must be numeric");
+        }
+        $this->memberID = $memberID;
+    }
+
+    private function setName($name) 
+    {
+        if ($name === "") {
+            throw new \Exception("Name cannot be blank");
+        }
+        $this->name = $name;
+    }
+
+    private function setSocialSecurityNumber($socialSecurityNumber) 
+    {
+        // Validation here
+        $this->socialSecurityNumber = $socialSecurityNumber;
     }
 }
