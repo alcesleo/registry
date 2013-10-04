b8107979be5031e271d3a8cb20b69e94554a315a
Xeald86 <js222xh@student.lnu.se>
2013-10-04 15:50:17 +0200

    CompactListView functional

--- src/Registry/Views/CompactMemberListView.php
+++ src/Registry/Views/CompactMemberListView.php
@@ -2,10 +2,49 @@
 
 namespace Registry\Views;
 
+use Registry\Models\MemberModel;
+use Registry\Models\ServiceModel;
+
+use PDO;
+
 class CompactMemberListView
 {
-    public function printMemberData() 
+    /**
+     * @var array $membersArray of MemberModel
+     * @var ServiceModel $service
+     */
+    private $memberModelArray;
+    private $service;
+
+    public function __construct()
     {
-        print ("Testing output for CompactMemberListView");
+        // Create databse and start service
+        $db = new PDO('sqlite:database/registry.sqlite');
+        $this->service = new ServiceModel($db);
+        
+        $this->memberModelArray = $this->service->getMembers();
+    }
+    
+    /**
+     * @var int $memberID
+     * @var string $name
+     * @var string $ssn
+     */
+    public function printMemberData()
+    {
+        print "     \n ----- Member Information -----";
+        
+        foreach ($this->memberModelArray as $obj) 
+        {
+            $memberID = $obj->getMemberID();
+            $name = $obj->getName();
+            $ssn = $obj->getSocialSecurityNumber();
+            
+            print "
+                    \n MemberID : $memberID
+                    \n Name : $name
+                    \n SSN : $ssn
+                    \n ------------------------------";
+        }
     }
 }
