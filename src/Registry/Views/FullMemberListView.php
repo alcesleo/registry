506d95865366d94897605f557eb09c68e0326a8e
elinlix <el222fr@student.lnu.se>
2013-10-06 01:02:05 +0200

    Fixed indentation with spaces

--- src/Registry/Views/FullMemberListView.php
+++ src/Registry/Views/FullMemberListView.php
@@ -49,20 +49,20 @@ class FullMemberListView
             print "\n\n\t---- Owned boats ----\n";
 
             if (count($boats)>0) {
-            	// http://stackoverflow.com/questions/7039010/how-to-make-alignment-on-console-in-php
-        		$pattern = "\t|%-3s |%-9s |%-6s |\n"; // Used in boat table for each member
+                // http://stackoverflow.com/questions/7039010/how-to-make-alignment-on-console-in-php
+                $pattern = "\t|%-3s |%-9s |%-6s |\n"; // Used in boat table for each member
 
-            	printf($pattern, "ID", "Boat type", "Length"); // Table header
-	            foreach ($boats as $boat) 
-	            {
-	            	$boatID = $boat->getBoatID();
-	            	$boatType = $boat->getBoatType(); // TODO: "Translate" the int to the type name??
-	            	$boatLength = $boat->getLength();
-	            	printf($pattern, $boatID, $boatType, $boatLength);
-	            }
-	        } else {
-	        	print "\tMember does not own any boats!";
-	        }
+                printf($pattern, "ID", "Boat type", "Length"); // Table header
+                foreach ($boats as $boat) 
+                {
+                    $boatID = $boat->getBoatID();
+                    $boatType = $boat->getBoatType(); // TODO: "Translate" the int to the type name??
+                    $boatLength = $boat->getLength();
+                    printf($pattern, $boatID, $boatType, $boatLength);
+                }
+            } else {
+                print "\tMember does not own any boats!";
+            }
         }
     }
 }
