e2a00b79ccb044e5dcef884286af67448580f4a6
alcesleo <lagginglion@gmail.com>
2013-10-07 16:23:54 +0200

    Removing testing code, fix standard

--- src/Registry/Views/CompactMemberListView.php
+++ src/Registry/Views/CompactMemberListView.php
@@ -3,6 +3,7 @@
 namespace Registry\Views;
 
 use Registry\Models\MemberModel;
+use Registry\Models\ServiceModel;
 
 class CompactMemberListView
 {
@@ -14,16 +15,14 @@ class CompactMemberListView
     private $serviceModel;
 
     /**
-     * @param \Registry\Models\ServiceModel $serviceModel 
+     * @param ServiceModel $serviceModel
      */
-    public function __construct(\Registry\Models\ServiceModel $serviceModel)
-    {        
+    public function __construct(ServiceModel $serviceModel)
+    {
         $this->serviceModel = $serviceModel;
-        // TODO: Change the call to getAllMembersWithBoats after its been implemented. 
-        //$this->memberModelArray = $this->service->getAllMembersWithBoats();
-        $this->memberModelArray = $this->serviceModel->getMembers();
+        $this->memberModelArray = $this->serviceModel->getMembersWithBoats();
     }
-    
+
     /**
      * @var int $memberID
      * @var string $name
@@ -32,18 +31,17 @@ class CompactMemberListView
     public function printMemberData()
     {
         print "\n ----- Member list -----\n\n";
-        
+
         // http://stackoverflow.com/questions/7039010/how-to-make-alignment-on-console-in-php
         $pattern = "|%-3s |%-25s |%-11s |%-12s |\n";
         printf($pattern, 'ID', 'Name', 'SSN', 'Nr. of boats'); // Table header
 
-        foreach ($this->memberModelArray as $obj) 
-        {
+        foreach ($this->memberModelArray as $obj) {
             $memberID = $obj->getMemberID();
             $name = $obj->getName();
             $ssn = $obj->getSocialSecurityNumber();
             $boats = $obj->getOwnedBoats();
-            
+
             printf($pattern, $memberID, $name, $ssn, count($boats));
         }
     }
