14b8f01b10fcb89d53d01e0c48dbdc30fd415a0f
alcesleo <lagginglion@gmail.com>
2013-10-09 12:55:05 +0200

    Prototype enum

--- src/Registry/Models/BoatTypeModel.php
+++ src/Registry/Models/BoatTypeModel.php
@@ -8,5 +8,78 @@ class BoatTypeModel
     const MOTORBOAT = 2;
     const MOTORSAILBOAT = 3;
     const CANOE = 4;
-    const OTHERS = 5;
+    const OTHER = 5;
+
+    /**
+     * String representations of types
+     * @var array
+     */
+    private static $strings = array(
+        1 => 'Sailboat',
+        2 => 'Motorboat',
+        3 => 'Motorsailboat',
+        4 => 'Canoe',
+        5 => 'Other'
+    );
+
+    /**
+     * @var int
+     */
+    private $boatType;
+
+    /**
+     * @return BoatTypeModel
+     */
+    public static function sailBoat()
+    {
+        return new BoatTypeModel(BoatTypeModel::SAILBOAT);
+    }
+
+    /**
+     * @return BoatTypeModel
+     */
+    public static function motorBoat()
+    {
+        return new BoatTypeModel(BoatTypeModel::MOTORBOAT);
+    }
+
+    /**
+     * @return BoatTypeModel
+     */
+    public static function motorSailBoat()
+    {
+        return new BoatTypeModel(BoatTypeModel::MOTORSAILBOAT);
+    }
+
+    /**
+     * @return BoatTypeModel
+     */
+    public static function canoe()
+    {
+        return new BoatTypeModel(BoatTypeModel::CANOE);
+    }
+
+    /**
+     * @return BoatTypeModel
+     */
+    public static function other()
+    {
+        return new BoatTypeModel(BoatTypeModel::OTHER);
+    }
+
+    /**
+     * @param int $boatTypeId one of the constants
+     */
+    private function __construct($boatType)
+    {
+        $this->boatType = $boatType;
+    }
+
+    /**
+     * @return string name of boat-type
+     */
+    public function __toString()
+    {
+        return self::$strings[$this->boatType];
+    }
 }
