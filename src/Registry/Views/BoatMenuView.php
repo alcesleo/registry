cbd6d1e0379b85e61f93d2106cfc405ddbd6f15c
Xeald86 <js222xh@student.lnu.se>
2013-10-09 15:05:06 +0200

    Gettign there, still in the works

---
+++ src/Registry/Views/BoatMenuView.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Registry\Views;
+
+use Exception;
+
+class BoatMenuView
+{
+    /**
+     * @var MenuView $view
+     */
+    private $view;
+    
+    /**
+     * @var array $options
+     */
+    private $options;
+
+
+    public function __construct()
+    {
+        $this->options = array(
+            'a' => 'Add boat',
+            'c' => 'Change boat',
+            'r' => 'Remove boat'
+        );
+
+        $this->view = new MenuView($this->options, "-----------\n Boat menu \n-----------");
+    }
+    
+    public function showMenuOptions() 
+    {
+        $input = $this->view->readMenuOption();
+    }
+}
