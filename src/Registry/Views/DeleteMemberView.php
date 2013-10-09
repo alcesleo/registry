a26b462db2a9d584441085a146e09e056f7916e7
alcesleo <lagginglion@gmail.com>
2013-10-09 10:28:29 +0200

    Fix standard

--- src/Registry/Views/DeleteMemberView.php
+++ src/Registry/Views/DeleteMemberView.php
@@ -10,29 +10,30 @@ class DeleteMemberView extends CommandLineView
      * @param MemberModel $member
      * @return Boolean
      */
-    public function userWantsToDeleteMember(MemberModel $member) 
+    public function userWantsToDeleteMember(MemberModel $member)
     {
         print "\n\nAttention: This will delete ". $member->getName() ." and boats registered to this member!\n";
-        
+
         $keepAsking = true;
         while ($keepAsking) {
             $input = $this->readLine("Do you want to delete this member? [Y/N]: ");
-            if(strtolower($input) == 'y' OR strtolower($input) == 'n')
+            if (strtolower($input) == 'y' or strtolower($input) == 'n') {
                 $keepAsking = false;
-            else
+            } else {
                 print "\n\nYou need to answer Yes(y) or No(n)\n";
+            }
         }
-        
+
         return ($input == 'y');
-    }  
-    
-    public function memberDeleted() 
+    }
+
+    public function memberDeleted()
     {
         print "\n\nMember was successfully deleted!\n";
-    }  
-    
-    public function memberNotDeleted() 
+    }
+
+    public function memberNotDeleted()
     {
         print "\n\nMember was not deleted!\n";
-    }   
+    }
 }
