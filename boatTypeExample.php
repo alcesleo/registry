059f57028afa09a08bda0855207e9c67246444b1
alcesleo <lagginglion@gmail.com>
2013-10-09 14:58:30 +0200

    Make examples of BoatTypeModel

--- boatTypeExample.php
+++ boatTypeExample.php
@@ -2,11 +2,25 @@
 <?php
 /**
  * This file is meant to test and demonstrate use of the BoatTypeModel
+ * It is meant to work as much as an Enum as possible
  */
 require('./vendor/autoload.php');
 
 use Registry\Models\BoatTypeModel as BoatType;
+use Registry\Models\BoatModel;
 
-$sailBoat = BoatType::sailBoat();
+// Create a type
+$sailBoatType = BoatType::SailBoat();
+print $sailBoatType . "\n";
 
-print $sailBoat;
+// Use it with a boat
+$boat = new BoatModel(null, $sailBoatType, 6);
+print $boat->getBoatType() . "\n";
+
+// Set boatType with constructor-function
+$boat->setBoatType(BoatType::Canoe());
+print $boat->getBoatType() . "\n";
+
+// Set boatType with constant
+$boat->setBoatTypeByID(BoatType::MOTORSAILBOAT);
+print $boat->getBoatType() . "\n";
